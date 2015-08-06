4e40140addcb7e402aeea75fee256af9bc9e6b14
Omar Shaikh <oshaikh13@gmail.com>
2015-08-06 14:05:25 -0700

    Rearrange files, and create modules for clarity. Add base Auth

--- app.js
+++ app.js
@@ -7,12 +7,39 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-// var IO = require('socket.io');
-var easySocket = require('./easySocket.js');
+var db = require('./schema.js');
+var easySocket = require('./lib/easySocket.js');
+var handler = require('./lib/socketHandler.js');
+var passport = require('passport');
+var session      = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
+var LocalStrategy = require('passport-local').Strategy;
+
+// This config should go in a module or something...
+var passportConfig = require('./lib/strategyConfig.js');
+passport.use(passportConfig);
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      db.User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    }
+));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('html', require('./lib/htmlEngine'));
+app.set('view engine', 'html');
+
 
 server.listen(8080);
 
@@ -21,13 +48,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({ secret: 'swagillrektum8' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+require('./lib/routes.js')(app, passport, db, bcrypt);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
 easySocket(server).onConnect(function(socket){
-  console.log('server ready')
-  socket.sendAll('news', {hello: "world"});
+  handler(socket);
 });
-
-
-
--- public/javascripts/easySocket.js
+++ public/javascripts/easySocket.js
@@ -2,6 +2,13 @@
 var easyio = {
   // Returns an io connection (provided by socket io) and attaches helpers
   connect: function(url){
+
+    if (url.split(':').length <= 2 ) {
+      url += ":" + window.location.port;
+      console.log('appended');
+      console.log(url);
+    }
+
     return {
       ioConnection: io.connect(url),  //Socket io creates this io obj... We are using it.
 
